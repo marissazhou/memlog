ca6b40fb31f7026da783767855996076a0712171
Marissa Zhou <marissa.zhou.cn@gmail.com>
2014-10-13 13:26:18 +0100

    Remove sh

--- cp_grant.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo cp /home/marissa/Documents/projects/memorymesh/sensecam_browser/templates/lifelog_view/includes/calender.html sensecam_browser/templates/lifelog_view/includes/
-sudo cp /home/marissa/Documents/projects/memorymesh/sensecam_browser/templates/lifelog_view/includes/image_view.html sensecam_browser/templates/lifelog_view/includes/
-
-sudo cp /home/marissa/Documents/projects/memorymesh/sensecam_browser/static/sensecambrowser/js/annotater.js sensecam_browser/static/sensecambrowser/js/
--- cp_out.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo cp sensecam_browser/templates/lifelog_view/includes/calender.html /home/git/www/sensecam_browser/sensecam_browser/templates/lifelog_view/includes/
-sudo cp sensecam_browser/templates/lifelog_view/includes/image_view.html /home/git/www/sensecam_browser/sensecam_browser/templates/lifelog_view/includes/
-
-sudo cp sensecam_browser/static/sensecambrowser/js/annotater.js /home/git/www/sensecam_browser/sensecam_browser/static/sensecambrowser/js/
